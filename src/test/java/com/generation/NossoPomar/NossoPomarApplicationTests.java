bf3edf92b84a01acd42d70a569f580b67c300dc2
JuanLucca846 <lucca470@gmail.com>
2024-04-01 10:06:54 -0300

    Add: Inicio do Projeto Integrador

---
+++ src/test/java/com/generation/NossoPomar/NossoPomarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.NossoPomar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NossoPomarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
